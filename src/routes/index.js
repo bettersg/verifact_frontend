6270ecc42622aff132bf8c398d1e188b08cd334e
T31K <t31kmunwong@gmail.com>
2021-03-14 16:38:38 +0800

    Fix register route

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import Question from "../views/Question";
 import Home from "../views/Home";
+import Register from "../views/Register";
 import NotFound from "../views/404";
 
 export default [
@@ -12,5 +13,6 @@ export default [
     component={Question}
   />,
   <Route exact key="home" path="/" component={Home} />,
+  <Route exact key="register" path="/register" component={Register} />,
   <Route key="404" path="*" component={NotFound} />,
 ];
