f1a5e131af0d335ec8cb55ca6216f9734aa7d785
T31K <t31kmunwong@gmail.com>
2021-03-20 14:01:43 +0800

    Modify exports

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,7 +1,8 @@
 import * as Text from './Text'
 import * as Button from './Button'
 import * as Input from './Input'
+import * as Layout from './Layout'
 
 export {
-  Text, Button, Input
+  Text, Button, Input, Layout
 }
