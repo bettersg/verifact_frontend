6cd1b95f29a8845237c8e5ee10ec2850c06abec2
T31K <t31kmunwong@gmail.com>
2021-03-22 16:27:39 +0800

    Added FormControl style

--- src/styles/Layout.js
+++ src/styles/Layout.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
+import { Form } from 'react-bootstrap'
 
 export const PageWrap = styled.div`
   padding: 0 16.6rem;
@@ -28,4 +29,19 @@ export const FormWrap = styled.div`
   @media only screen and (max-width: 767px){
     width: 96%;
   }
+  
+`
+export const FormControl = styled(Form.Control)`
+  height: 45px;
+  background: #FFFFFF !important;
+  border: 1px solid #E5E5E5;
+  box-sizing: border-box;
+  border-radius: 10px;
+  padding: 15px;
+  font-family: 'Open Sans', sans-serif;
+  color: #464646;
+  font-style: normal;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 22px;
 `
