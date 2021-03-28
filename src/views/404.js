aed2159950d70e459ca80c126ba0f0ba100b8f19
fabianchua6 <fabianchua6@gmail.com>
2021-03-29 01:34:45 +0800

    update page 404

--- src/views/404.js
+++ src/views/404.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import Logo from '../assets/VeriFactLogo.svg';
 
-export default function NotFound (props) {
-  return (
-    <div>
-      <h1>404 page</h1>
-    </div>
-  )
+export default function NotFound(props) {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				flexDirection: 'column',
+				textAlign: 'center',
+				alignItems: 'center',
+				height: '100vh'
+			}}
+		>
+			<h1 style={{ fontSize: '6rem' }}>
+				Error 404! <br />Page Not Found!
+			</h1>
+			<p style={{ fontSize: '3rem', lineHeight:'normal' }}>
+				Seems like you're lost, <br />follow this little bee to return home.
+			</p>
+			<a href="/">
+				<img src={Logo} alt="VeriFact Logo" height="45rem" />
+			</a>
+		</div>
+	);
 }
