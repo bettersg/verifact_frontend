c48164038c554687e5b1a412f87a15b513c45393
fabianchua6 <fabianchua6@gmail.com>
2021-04-05 18:08:27 +0800

    fixed 404.js formatting and remove <br/>

--- src/views/404.js
+++ src/views/404.js
@@ -1,27 +1,27 @@
-import React from 'react';
-import Logo from '../assets/VeriFactLogo.svg';
+import React from 'react'
+import Logo from '../assets/VeriFactLogo.svg'
 
-export default function NotFound(props) {
-	return (
-		<div
-			style={{
-				display: 'flex',
-				justifyContent: 'center',
-				flexDirection: 'column',
-				textAlign: 'center',
-				alignItems: 'center',
-				height: '100vh'
-			}}
-		>
-			<h1 style={{ fontSize: '6rem' }}>
-				Error 404! <br />Page Not Found!
-			</h1>
-			<p style={{ fontSize: '3rem', lineHeight:'normal' }}>
-				Seems like you're lost, <br />follow this little bee to return home.
-			</p>
-			<a href="/">
-				<img src={Logo} alt="VeriFact Logo" height="45rem" />
-			</a>
-		</div>
-	);
+export default function NotFound (props) {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        flexDirection: 'column',
+        textAlign: 'center',
+        alignItems: 'center',
+        height: '100vh'
+      }}
+    >
+      <h1 style={{ fontSize: '6rem' }}>
+        Page Not Found!
+      </h1>
+      <p style={{ fontSize: '3rem', lineHeight: 'normal' }}>
+        Seems like you're lost, follow this little bee to return home.
+      </p>
+      <a href='/'>
+        <img src={Logo} alt='VeriFact Logo' height='45rem' />
+      </a>
+    </div>
+  )
 }
