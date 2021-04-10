fa363caf177b6f558b2aa4905c6859c0f75653b4
T31K <t31kmunwong@gmail.com>
2021-04-10 22:51:48 +0800

    Added context provider to wrap around entire app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { BrowserRouter as Router, Switch } from 'react-router-dom'
-import { AuthProvider, AuthConsumer } from './context/Auth'
+import { AuthProvider } from './context/Auth'
 
 import routes from './routes'
 import Navbars from './components/Navbar'
 
 export default function App () {
   return (
-        <Router>
-          <Navbars />
-          <main>
-            <Switch>{routes}</Switch>
-          </main>
-        </Router>
+    <AuthProvider>
+      <Router>
+        <Navbars />
+        <main>
+          <Switch>{routes}</Switch>
+        </main>
+      </Router>
+    </AuthProvider>
   )
 }
+
+// context - global variable
